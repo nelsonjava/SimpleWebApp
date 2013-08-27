92f1b3f18e2744ede07012339e4e40edf553f61a
naif <nelsonjava@gmail.com>
2013-08-27 15:18:37 -0500

    first commit

---
+++ src/test/SeleniumHelloWorldExample.java
@@ -0,0 +1,36 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleniumException;
+
+public class SeleniumHelloWorldExample extends TestCase {
+  private DefaultSelenium selenium;
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    selenium = createSeleniumClient("http://localhost:8080/");
+    selenium.start();
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    selenium.stop();
+    super.tearDown();
+  }
+
+  protected DefaultSelenium createSeleniumClient(String url) throws Exception {
+    return new DefaultSelenium("localhost", 4444, "*firefox", url);
+  }
+  
+  @Test
+  public void testHelloWorld() throws Exception {
+    try {
+      selenium.open("http://localhost:8080/mywebapp/index.jsp");
+      assertEquals("Hello, World", selenium.getText("//h1"));
+    } catch (SeleniumException ex) {
+      fail(ex.getMessage());
+      throw ex;
+    }
+  }
+}
